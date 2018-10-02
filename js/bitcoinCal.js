5e388733da917aeefd489f830699dfef34e37e71
Tunde Osborne <osbornetunde@gmail.com>
2018-10-02 13:34:00 +0100

    refactoring

--- js/bitcoinCal.js
+++ js/bitcoinCal.js
@@ -1,30 +1,4 @@
-/*const btc = 3.5;
-const btcUsedBought = 10000;
-const btcUsedNow = 12000;
 
-const invested = btc * btcUsedBought;
-const current = btc * btcUsedNow;
-
-
-const profit = current - invested;
-//const growth = profit / invested * 100;
-const growth = calGrowth(invested, current);
-
-function calGrowth(pastVal, currentVal) {
-	return  (currentVal - pastVal) / pastVal * 100;
-}
-
-
-let message = "";
-if (profit > 0){
-	message = `Great you made a profit of $ ${profit} (${growth}%).`;
-} else if (profit < 0){
-	message = `Oh no! You're at a loss of $${profit} (${growth}%).`;
-} else {
-	message = "You're breaking even!";
-}
-
-console.log(message); */
 
 const calGrowth = (pastVal, currentVal) => (currentVal - pastVal) / pastVal * 100;
 
